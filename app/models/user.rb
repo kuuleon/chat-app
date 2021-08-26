783157ca48b07e09078bc1284b9342747773fb92
kuuleon <tudopassa427911@gmail.com>
2021-08-26 15:43:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  #ユーザー登録時に「name」を空欄にして登録しようとすると、エラーが発生
 end
