0c42a5a10237e3289e9476aea34436ccba30ff9a
kuuleon <tudopassa427911@gmail.com>
2021-08-27 10:50:57 +0900

    コメントアウトの挿入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
-  def edit   
+  def edit   #ビューファイルを表示するだけなので、アクションの定義のみ
   end
-  def update
-    if current_user.update(user_params) 
+
+  def update #Userモデルの更新を行うアクション
+    if current_user.update(user_params) #ログインしているユーザーの情報を更新
         redirect_to root_path #成功した場合root（チャット画面）にリダイレクト
     else
        render :edit #失敗した場合、editのビューが表示される
@@ -14,6 +15,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email)
+    #ストロングパラメーターを使用し、user_paramsを定義します。
+    #user_paramsの中でpermitメソッドを使用し、nameとemailの編集を許可
   end
 
 end
