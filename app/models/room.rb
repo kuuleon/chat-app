4f6950ea031b25e3ac0f6f0f5a8964595d905841
kuuleon <tudopassa427911@gmail.com>
2021-08-31 12:04:10 +0900

    messageテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users 
   #roomsテーブルはroom_usersテーブル（中間テーブル）をとうしてusersテーブルをたくさん持っている
+  has_many :messages
   
   validates :name, presence:true
   #チャットルームを新規作成するにあたってルーム名が存在している場合のみ作成可
