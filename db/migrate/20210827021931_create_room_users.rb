729b1674ade8310d89b6ce47047df1ac6f35100d
kuuleon <tudopassa427911@gmail.com>
2021-08-31 11:39:11 +0900

    コメントアウトの修正

--- db/migrate/20210827021931_create_room_users.rb
+++ db/migrate/20210827021931_create_room_users.rb
@@ -5,7 +5,8 @@ class CreateRoomUsers < ActiveRecord::Migration[6.0]
       t.references :user, null: false, foreign_key: true
       t.references :room, null: false, foreign_key: true
       #t.references :userと記載すれば、user_idというカラムが生成
-      #ここに保存する「ユーザー」と「チャットグループ」は必ず存在している事が前提なので、「usersテーブル」と「roomsテーブル」の情報を参照する
+      #ここに保存する「ユーザー」と「チャットグループ」は必ず存在している事が前提なので、
+      #「usersテーブル」と「roomsテーブル」の情報を参照する
       #参照する役割を果たすのが「foreign_key: true」
       t.timestamps
     end
