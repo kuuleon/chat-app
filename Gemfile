9ac4dfbc375db94e214cc66410bc77fd0d2d4114
kuuleon <tudopassa427911@gmail.com>
2021-09-01 12:32:11 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise' #deviseのインストール
 gem 'pry-rails'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,7 +29,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
   #privateメソッドとしてmessage_paramsを定義し、
   #メッセージの内容contentをmessagesテーブルへ保存できるようにします。
